3ef9e0bdb422c23f960e54a0d30c56129d87a826
Shawn Pearson <1157wcl@gmail.com>
2023-09-22 14:54:07 -0400

    export code to customMdx component

--- app/content/[[...slug]]/page.js
+++ app/content/[[...slug]]/page.js
@@ -1,9 +1,5 @@
-import { notFound } from "next/navigation";
-import Image from "next/image";
-import { CopyButton } from "@/components/copycode";
 import { allContents } from "contentlayer/generated";
 import { CustomMdx } from "@/components/custom-mdx";
-import { useMDXComponent } from "next-contentlayer/hooks";
 
 // console.log(allContents);
 export async function generateStaticParams() {
@@ -16,37 +12,16 @@ export const generateMetadata = ({ params }) => {
   // TODO: other possible good metadatasasasz
   return { title: "SCW: " + content.title };
 };
-// TODO: move to another file
-const Pre = ({ children, raw, ...props }) => {
-  const lang = props["data-language"] || "shell";
-  return (
-    <pre {...props} className={"p-0"}>
-      <div className={"code-header"}>
-        {lang}
-        <CopyButton text={raw} />
-      </div>
-      {children}
-    </pre>
-  );
-};
-
-const components = {
-  Image,
-  pre: Pre,
-};
-// Move above to another file
 
 const ContentLayout = ({ params }) => {
   const urlSlug = `${params.slug.join("/")}`;
   const content = allContents.find((item) => item._raw.flattenedPath === urlSlug);
-  // console.log(content.body.code);
-  const Content = useMDXComponent(content.body.code);
   return (
     <article className="py-8 mx-auto max-w-xl">
       <div className="mb-8 text-center">
         <h1>{content.title}</h1>
       </div>
-      <Content components={components} />
+      <CustomMdx code={content.body.code} />
     </article>
   );
 };
