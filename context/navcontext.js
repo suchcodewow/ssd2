268c21792a270e2a613e589d5dccbbd963e7086e
Shawn Pearson <1157wcl@gmail.com>
2023-09-26 16:38:42 -0400

    tweaks

--- context/navcontext.js
+++ context/navcontext.js
@@ -7,11 +7,13 @@ export default function NavLink({ navItem }) {
   // console.log(navItem);
   const currentPath = usePathname();
   const cleanSlug = navItem.url.endsWith("/") ? navItem.url.slice(0, -1) : navItem.url;
-  // console.log("segment:", currentPath, "slug:", cleanSlug);
+  // console.log("segment:", currentPath, "slug:", cleanSlug.substring(0, currentPath.length));
   const isActive = cleanSlug === currentPath;
+  const isOnPath = cleanSlug.substring(0, currentPath.length) === currentPath;
+  console.log(cleanSlug, "isOnPath:", isOnPath);
 
   return (
-    <div key={navItem.url}>
+    <div key={navItem.url} className={isOnPath ? "bg-slate-300" : ""}>
       <Link
         href={navItem.url}
         // Change style depending on whether the link is active
