d0d138a99277f4702f9aad7e1178f96a9e0d467b
Shawn Pearson <1157wcl@gmail.com>
2023-10-04 17:32:25 -0400

    mid

--- context/sidenav.jsx
+++ context/sidenav.jsx
@@ -13,63 +13,43 @@ export const RenderNav = ({ structure, docs }) => {
   // console.log(structure.map((a) => a.url));
   const currentUri = usePathname();
   const uriStructureData = structure.find((a) => a.url === currentUri);
-  const sideNavdata = structure.filter(
-    (a) => a.section === uriStructureData.section || a.parentOf === uriStructureData.section
-  );
+  const normalizedSection = uriStructureData.parentOf ? uriStructureData.parentOf : uriStructureData.section;
+  const sideNavdata = structure.filter((a) => a.section === normalizedSection || a.parentOf === normalizedSection);
   console.log(sideNavdata);
-  // if (parentOf) {
-  //   console.log("this is a root");
-  // } else {
-  //   console.log("file");
-  // }
-  // const currentNav = structure.find((a) => a.url == currentUri);
-  const navBase = uriStructureData.parentOf ? uriStructureData.parentOf : uriStructureData.section;
-  // console.log(navBase);
-  // if (structure.includes(contentBase)) {
-  //   console.log("root url");
-  // } else {
-  //   console.log("file in section");
-  // }
-  // console.log(contentBase);
-  // const { baseUrl, generateNav } = useContext(ClientContext);
-  // if (generateNav) {
-  //   console.log("generate: ", currentURL);
-  // }
+  // const navBase = uriStructureData.parentOf ? uriStructureData.parentOf : uriStructureData.section;
   return (
     <div className="mx-auto w-full max-w-md  bg-white dark:bg-slate-800 ml-2">
-      {docs
-        .filter((a) => a.section == navBase.section)
-        .map(
-          (navItem) => {
-            // if (navItem.parentOf) {
-            //   return [
-            //     <Disclosure key={navItem.url}>
-            //       {({ open }) => [
-            //         <>
-            //           <Disclosure.Button className="flex w-full justify-start bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-            //             {navItem.title}
-            //             <ChevronRightIcon className={`${open ? "rotate-90 transform" : ""} h-5 w-5 text-purple-500`} />
-            //           </Disclosure.Button>
-            //           <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-            //             <RenderNav nodeStart={navItem.parentOf} />
-            //           </Disclosure.Panel>
-            //         </>,
-            //       ]}
-            //     </Disclosure>,
-            //   ];
-            // } else {
-            return [
-              // <Disclosure>
-              //   <Disclosure.Button>{navItem.title}</Disclosure.Button>,<Disclosure.Panel>I dunno</Disclosure.Panel>
-              // </Disclosure>,
-              <Link key={navItem.url} href={navItem.url}>
-                {navItem.title}
-              </Link>,
-              <br />,
-            ];
-          }
-          //   return [<NavLink navItem={navItem} />, navItem.parentOf && <RenderNav nodeStart={navItem.parentOf} />];
-        )}
+      {sideNavdata.map(
+        (navItem) => {
+          // if (navItem.parentOf) {
+          //   return [
+          //     <Disclosure key={navItem.url}>
+          //       {({ open }) => [
+          //         <>
+          //           <Disclosure.Button className="flex w-full justify-start bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          //             {navItem.title}
+          //             <ChevronRightIcon className={`${open ? "rotate-90 transform" : ""} h-5 w-5 text-purple-500`} />
+          //           </Disclosure.Button>
+          //           <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+          //             <RenderNav nodeStart={navItem.parentOf} />
+          //           </Disclosure.Panel>
+          //         </>,
+          //       ]}
+          //     </Disclosure>,
+          //   ];
+          // } else {
+          return [
+            // <Disclosure>
+            //   <Disclosure.Button>{navItem.title}</Disclosure.Button>,<Disclosure.Panel>I dunno</Disclosure.Panel>
+            // </Disclosure>,
+            <Link key={navItem.url} href={navItem.url}>
+              {navItem.title}
+            </Link>,
+            <br />,
+          ];
+        }
+        //   return [<NavLink navItem={navItem} />, navItem.parentOf && <RenderNav nodeStart={navItem.parentOf} />];
+      )}
     </div>
   );
 };
